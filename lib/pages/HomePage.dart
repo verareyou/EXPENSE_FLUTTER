0195f8de82e047137aa57960f25e7eac0cdf8295
himanshu <hmxnsu.blvck@gmail.com>
2023-03-12 00:01:33 +0530

    ...

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // addNewExpense
+  static void addNewExpense() {
+    print('addNewExpense');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My First App"),
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 240, 248, 255),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewExpense,
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromARGB(255, 53, 57, 60),
+        foregroundColor: Color.fromARGB(255, 213, 213, 213),
       ),
-      body: const Center(
-        child: Text("This is my default text"),
-      ),
-      drawer: Drawer(),
     );
   }
 }
